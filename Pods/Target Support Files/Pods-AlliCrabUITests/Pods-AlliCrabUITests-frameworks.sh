f7c0e0a8f0f5218c8261dcc613dfd13dbc880883
Alan Garny <agarny@hellix.com>
2020-01-18 19:00:21 +1300

    pod update.

--- Pods/Target Support Files/Pods-AlliCrabUITests/Pods-AlliCrabUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-AlliCrabUITests/Pods-AlliCrabUITests-frameworks.sh
@@ -168,6 +168,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/FMDB/FMDB.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/sqlite3/sqlite3.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SimulatorStatusMagic/SimulatorStatusMagic.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
--- Pods/Target Support Files/sqlite3/sqlite3-umbrella.h
+++ Pods/Target Support Files/sqlite3/sqlite3-umbrella.h
@@ -11,7 +11,6 @@
 #endif
 
 #import "sqlite3.h"
-#import "sqlite3ext.h"
 
 FOUNDATION_EXPORT double sqlite3VersionNumber;
 FOUNDATION_EXPORT const unsigned char sqlite3VersionString[];
